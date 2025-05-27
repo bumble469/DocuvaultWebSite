dece400bd82ad28191871ae7dd4adefb5cbcd793
bumble469 <bumble469@gmail.com>
2025-05-27 23:55:33 +0530

    small change

--- src/context/DocumentContext.jsx
+++ src/context/DocumentContext.jsx
@@ -17,7 +17,6 @@ export const DocumentsProvider = ({ children }) => {
     setDocuments(null);
     setUserFileStorage(null);
     setLoading(false);
-    toast.error("Session expired! Please login again..");
   };
 
   const getUserStorage = async () => {
